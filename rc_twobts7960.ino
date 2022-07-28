55ba273bf38ee4546ab61c9974a65865f3d1cd20
Samiun Mahmud Arno <90166425+samiunarno@users.noreply.github.com>
2022-07-28 19:12:21 +0600

    Update rc_twobts7960.ino

--- rc_twobts7960.ino
+++ rc_twobts7960.ino
@@ -46,135 +46,135 @@ if (Serial.available() > 0) {
      case 'S':
       {
         stopo();
-       //Serial.println("Stop\n"); 
+       //Serial.println("Stop\n");
        incomingByte='*';}
      break;
      
      case 'L':
-      
+     
      {  left();
        
       // Serial.println("Forward\n");
        incomingByte='*';}
      break;
-    
-      case 'F':
+   
+      case 'R':
        
     {   right();
       // Serial.println("Backward\n");
        incomingByte='*';}
      break;
      
-     case 'R':
+     case 'F':
      // turn right
      {  
-       forward(); 
+       forward();
       // Serial.println("Rotate Right\n");
        incomingByte='*';}
      break;
        case 'B':
-      { 
-       backward();     
+      {
+       backward();    
        //Serial.println("Rotate Left\n");
        incomingByte='*';}
      break;
      case '1':
-        
-      { speed_left = 20; 
+       
+      { speed_left = 20;
        speed_right = 20;
        //Serial.println("Speed 1\n");
        incomingByte='*';}
      break;
     case '2':
-      { 
-        speed_left = 40; 
+      {
+        speed_left = 40;
        speed_right = 40;
        //Serial.println("Speed 2 \n");
        incomingByte='*';}
      break;
     case '3':
-      { 
-        speed_left = 60; 
+      {
+        speed_left = 60;
        speed_right = 60;
        //Serial.println("Speed 3 \n");
        incomingByte='*';}
-     break; 
+     break;
         case '4':
-      { 
-        speed_left = 80; 
+      {
+        speed_left = 80;
        speed_right = 80;
        //Serial.println("Speed 4 \n");
        incomingByte='*';}
-     break; 
+     break;
         case '5':
-      { 
-        speed_left = 100; 
+      {
+        speed_left = 100;
        speed_right = 100;
        //Serial.println("Speed 5 \n");
        incomingByte='*';}
-     break; 
+     break;
         case '6':
-      { 
-        speed_left = 120; 
+      {
+        speed_left = 120;
        speed_right = 120;
        //Serial.println("Speed 6 \n");
        incomingByte='*';}
-     break; 
+     break;
         case '7':
-      { 
-        speed_left = 140; 
+      {
+        speed_left = 140;
        speed_right = 140;
       // Serial.println("Speed 7 \n");
        incomingByte='*';}
-     break; 
+     break;
         case '8':
-      { 
-        speed_left = 160; 
+      {
+        speed_left = 160;
        speed_right = 160;
        //Serial.println("Speed 8 \n");
        incomingByte='*';}
-     break; 
+     break;
         case '9':
-      { 
-        speed_left = 200; 
+      {
+        speed_left = 200;
        speed_right = 200;
        //Serial.println("Speed 9 \n");
        incomingByte='*';}
-     break; 
+     break;
         case 'q':
-      { 
-        speed_left = 255; 
+      {
+        speed_left = 255;
        speed_right = 255;
        Serial.println("Speed full \n");
        incomingByte='*';}
-     break; 
-        case 'H':
-      { 
+     break;
+        case 'J':
+      {
        back_right();
        Serial.println("Speed full \n");
        incomingByte='*';}
      break;
-      case 'I':
-      { 
+      case 'H':
+      {
        back_left();
        Serial.println("Speed full \n");
        incomingByte='*';}
      break;
-      case 'G':
-      { 
+      case 'I':
+      {
        forward_right();
        Serial.println("Speed full \n");
        incomingByte='*';}
      break;
-      case 'J':
-      { 
+      case 'G':
+      {
       forward_left();
        Serial.println("Speed full \n");
        incomingByte='*';}
      break;      
   }
 
-  
+ 
 }
 void forward(){
 
@@ -200,8 +200,8 @@ void left(){
   analogWrite(L_PWM_FOR_ONE,0);
   analogWrite(R_PWM_FOR_TWO,speed_right);
   analogWrite(L_PWM_FOR_TWO,0);
-  
-  
+ 
+ 
   };
 void stopo(){
 
@@ -209,16 +209,16 @@ void stopo(){
   analogWrite(L_PWM_FOR_ONE,0);
   analogWrite(R_PWM_FOR_TWO,0);
   analogWrite(L_PWM_FOR_TWO,0);
-  
-  
+ 
+ 
   };
 void forward_left(){
    analogWrite(R_PWM_FOR_ONE,0);
   analogWrite(L_PWM_FOR_ONE,0);
   analogWrite(R_PWM_FOR_TWO,speed_right);
   analogWrite(L_PWM_FOR_TWO,0);
-  
-  
+ 
+ 
   };
 void forward_right(){
      analogWrite(R_PWM_FOR_ONE,0);
@@ -231,13 +231,13 @@ void back_left(){
   analogWrite(L_PWM_FOR_ONE,0);
   analogWrite(R_PWM_FOR_TWO,0);
   analogWrite(L_PWM_FOR_TWO,speed_right);
-  
+ 
   };
 void back_right(){
       analogWrite(R_PWM_FOR_ONE,speed_left);
   analogWrite(L_PWM_FOR_ONE,0);
   analogWrite(R_PWM_FOR_TWO,0);
   analogWrite(L_PWM_FOR_TWO,0);
-  
-  
+ 
+ 
   };
